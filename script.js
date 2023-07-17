168edfe6656aa8466b79784f0558604fbec3a982
Ayoub zouine <98323356+ayouzou@users.noreply.github.com>
2023-07-17 22:20:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let i=0;
+let j=4;
+const dots=document.querySelectorAll(".dot-container button")
+function next(){
+    document.getElementById("content" +(i+1)).classList.remove("active");
+    i = ( j + i + 1 ) % j;
+    document.getElementById("content"+(i+1)).classList.add("active");
+    indicateur(i+1);
+}
+function prev(){
+    document.getElementById("content"+(i+1)).classList.remove("active");
+    i=(j+i-1)%j;
+    document.getElementById("content"+(i+1)).classList.add("active");
+    indicateur(i+1);
+}
+function indicateur(num){
+    dots.forEach(function(dot){
+        dot.style.backgroundColor="transparent";
+    });
+    document.querySelector(".dot-container button:nth-child("+ num +")").style.backgroundColor="#8052ec"
+}
